d2996d9ed7ebc45ebc49daad9847a8ff642faaf9
roy1210 <rswkure@gmail.com>
2019-08-26 07:59:38 +0800

    Added clickable marker content

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -5,6 +5,7 @@ interface Mappable {
     lat: number;
     lng: number;
   };
+  markerContent(): string;
 }
 
 export class CustomMap {
@@ -22,12 +23,19 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    });
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
